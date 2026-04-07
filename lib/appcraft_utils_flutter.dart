162cf5fa5d303d2972bab3bd04c30452eddbc73e
Dmitriy Polyakov <dmitriyap11@gmail.com>
2026-04-07 12:45:25 +0300

    Release 0.0.3

--- lib/appcraft_utils_flutter.dart
+++ lib/appcraft_utils_flutter.dart
@@ -1,7 +1,9 @@
 library;
 
 export 'src/exceptions/exceptions.dart';
+export 'src/extensions/extensions.dart';
 export 'src/inputs/inputs.dart';
 export 'src/localization/localization.dart';
+export 'src/mappers/mappers.dart';
 export 'src/models/models.dart';
 export 'src/notifier/notifier.dart';
